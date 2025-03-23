825698594f299354186b5099dc8341eb9e5cd15b
salim soy <salimsoy0320@gmail.com>
2025-03-24 01:04:59 +0300

    görüntü griye çevrilip gri görüntü kaydedildi

--- opencv.py
+++ opencv.py
@@ -1,8 +1,12 @@
 import cv2 as cv
 
 img = cv.imread("dog.jpg")
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
+cv.imshow("gri resim", gray)
 cv.imshow("orjinal goruntu", img)
+
+cv.imwrite("graydog.jpg", gray)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
