24e35b70f7fc15c11bb2882aa5bd68b6aeed09c6
salim soy <salimsoy0320@gmail.com>
2025-04-04 16:12:39 +0300

    open cv ile görüntüyü belli açılarda döndürme kod dosyası eklendi

---
+++ dondurme.py
@@ -0,0 +1,20 @@
+import cv2 as cv 
+
+img = cv.imread("dog.jpg")
+
+h, w, _ = img.shape
+
+center = (w // 2, h // 2)
+angle = 90
+scale = 1.0
+
+
+transform_matrix = cv.getRotationMatrix2D(center, angle, scale)
+
+rotate = cv.warpAffine(img, transform_matrix, (w, h))
+
+cv.imshow("orginal", img)
+cv.imshow("rotated", rotate)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
